2afe65284a53cfa0a7f149b8b2b3abd3db314810
n.m.borisova <K8sedrf@1q>
2024-06-04 09:38:25 +0300

    Initial commit

---
+++ src/bg/smg/Main.java
@@ -0,0 +1,9 @@
+package bg.smg;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    Test1 t = new Test1();
+
+    }
+}
---
+++ src/bg/smg/Test1.java
@@ -0,0 +1,25 @@
+package bg.smg;
+
+public class Test1 {
+
+    // Case 1: Static variable
+    static int i;
+    // Case 2: Non-static variable
+    int j;
+
+    // Case 3: Static blocks
+    // static initialization block
+    {
+        i = 10;
+        System.out.println("static block called ");
+    }
+
+    // static initialization block
+    {
+        j=2;
+        System.out.println("int");
+    }
+
+    // Constructor calling
+    Test1() { System.out.println("Constructor called"); }
+}
